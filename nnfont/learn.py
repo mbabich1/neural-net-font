697f93f0dc8105200a9cd2e9e1cee8dfde83bb0f
Michael Babich <mbabich1@umbc.edu>
2026-05-14 18:18:19 -0400

    Add more stub functions and classes to fill in

--- nnfont/learn.py
+++ nnfont/learn.py
@@ -1,4 +1,14 @@
 import torch
+from torch import nn, tensor
+
+class StubModule(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        # self.layers = nn.ModuleList(...)
+        # self.fns = nn.ModuleList(...)
+
+    def forward(self, x):
+        pass
 
 def main():
     pass
---
+++ nnfont/rasterize_font.py
@@ -0,0 +1,7 @@
+import freetype
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
