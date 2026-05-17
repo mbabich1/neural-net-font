dc9c5d6063ecc1cbef87265a9ffb75b7f24a6bcc
Michael Babich <mbabich1@umbc.edu>
2026-05-16 20:52:52 -0400

    Move the plotting to its own function

--- nnfont/visualize.py
+++ nnfont/visualize.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import rasterize_font
 
-def main():
+def plot_word(words, word_id):
     plt.style.use('_mpl-gallery-nogrid')
-    word = rasterize_font.load_words_as_tensor(n = 2000)[1234]
+    word = words[word_id]
     fig, ax = plt.subplots()
     ax.imshow(word, origin='upper')
     plt.show()
+
+def main():
+    plot_word(rasterize_font.load_words_as_tensor(n = 2000), 1234)
 
 if __name__ == "__main__":
     main()
