27dd2a0287a7746b2e2f3fb865a84b9e2c0e9eb9
Michael Babich <mbabich1@umbc.edu>
2026-05-16 21:09:00 -0400

    Have two test plots

--- nnfont/visualize.py
+++ nnfont/visualize.py
@@ -14,6 +14,9 @@ def main():
     words, x_size = rasterize_font.flatten_words(rasterize_font.load_words_as_tensor(n = 2000))
     words = rasterize_font.unflatten_words(words, x_size)
     plot_word(words, 1234)
+    # And now let's plot a larger size.
+    words = rasterize_font.load_words_as_tensor(n = 2000, size=12)
+    plot_word(words, 1234)
 
 if __name__ == "__main__":
     main()
