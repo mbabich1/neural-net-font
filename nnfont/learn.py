1aed5332ff73d85803adec69a58c76aab1426db4
Jonathan Eanes <eanesj1123@gmail.com>
2026-05-18 15:57:24 -0400

    add saving to each epoch

--- nnfont/learn.py
+++ nnfont/learn.py
@@ -215,10 +215,12 @@ def train(
             img_id=f"out/serif_bold_italic_{epoch}.png"
         )
 
-    torch.save({
-        "generator_state_dict": generator.state_dict(),
-        "discriminator_state_dict": discriminator.state_dict(),
-    }, "model_state_dicts.pt")
+        generator.train()
+
+        torch.save({
+            "generator_state_dict": generator.state_dict(),
+            "discriminator_state_dict": discriminator.state_dict(),
+        }, f"model_state_dicts_{epoch}.pt")
 
 
 
